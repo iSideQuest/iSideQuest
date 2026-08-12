9341ca5d034cbd2349655620ddc7c2e59c16a2f8
iSideQuest <loot-shelf@outlook.com>
2026-08-12 17:54:42 -0600

    Add files via upload

--- SideKick/sw.js
+++ SideKick/sw.js
@@ -1,6 +1,6 @@
 // SideKick Service Worker
 
-const CACHE_NAME = 'sidekick-v25';
+const CACHE_NAME = 'sidekick-v26';
 
 // Files to cache on install — the core app shell (relative paths)
 const PRECACHE = [
