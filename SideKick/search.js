70cc38ae480a9b8cd1d848685708102081a07747
iSideQuest <loot-shelf@outlook.com>
2026-08-19 21:39:42 -0600

    Add files via upload

--- SideKick/search.js
+++ SideKick/search.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const lines = fs.readFileSync('index.html', 'utf8').split('\n');
-const needles = ['random-first', 'duel-1st', 'duel1st', 'randomFirst', 'first-opt'];
 lines.forEach((l, i) => {
-  needles.forEach(n => {
-    if (l.includes(n)) console.log((i + 1) + ' [' + n + ']: ' + l.trim().slice(0, 110));
-  });
+  if (l.includes('img/svg/') && (l.includes('LArrow') || l.includes('RArrow') || l.includes('PassLeft') || l.includes('PassRight'))) {
+    console.log((i + 1) + ': ' + l.trim().slice(0, 150));
+  }
 });
