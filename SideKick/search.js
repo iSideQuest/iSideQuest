d004e071c512d072fbb0dd4a7aa547b072f37a18
iSideQuest <loot-shelf@outlook.com>
2026-08-21 09:13:30 -0600

    Delete SideKick/search.js

--- SideKick/search.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('index.html', 'utf8').split('\n');
-lines.forEach((l, i) => {
-  if (l.includes('img/svg/') && (l.includes('LArrow') || l.includes('RArrow') || l.includes('PassLeft') || l.includes('PassRight'))) {
-    console.log((i + 1) + ': ' + l.trim().slice(0, 150));
-  }
-});
